76f05d7c4c3589f7ba9580f49d4c78b3c0f67036
Bima Satria <ppicbimasatria@gmail.com>
2024-04-02 23:30:48 +0700

    Mengubah product page

--- resources/products.php
+++ resources/products.php
@@ -2,24 +2,28 @@
 
 $datas = [
     'P-001' => [
+        "id"    => 'P-001',
         "title" => "Isyana",
         "release-date" => "26 Mei 2023",
         "cover" => "https://upload.wikimedia.org/wikipedia/id/thumb/5/5b/Isyana_Sarasvati_-_Isyana.jpg/220px-Isyana_Sarasvati_-_Isyana.jpg",
         "label" => "Redrose Records",
     ],
     'P-002' => [
+        "id"    => 'P-002',
         "title" => "Lexicon",
         "release-date" => "29 November 2019",
         "cover" => "https://upload.wikimedia.org/wikipedia/id/thumb/2/20/LEXICON_Isyana_Sarasvati.jpg/220px-LEXICON_Isyana_Sarasvati.jpg",
         "label" => "Sony Music Entertainment Indonesia",
     ],
     'P-003' => [
+        "id"    => 'P-003',
         "title" => "Paradox",
         "release-date" => "1 September 2017",
         "cover" => "https://upload.wikimedia.org/wikipedia/id/thumb/7/71/Paradox_%28album_music_by_Isyana_Sarasvati%29.jpg/220px-Paradox_%28album_music_by_Isyana_Sarasvati%29.jpg",
         "label" => "Sony Music Entertainment Indonesia",
     ],
     'P-004' => [
+        "id"    => 'P-004',
         "title" => "Explore!",
         "release-date" => "25 November 2015",
         "cover" => "https://is1-ssl.mzstatic.com/image/thumb/Music6/v4/cc/ed/53/cced53d1-ea13-12df-119c-3819a30e0f60/886445558678.jpg/316x316bb.webp",
